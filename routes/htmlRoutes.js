e29d64290e93c500f55eb57aeba9720895771d48
unknown <chugm1989@gmail.com>
2020-02-16 10:43:28 -0500

    added 'about' and 'contact' page skeletons

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,15 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, '../public/views/index.html'));
     });
+
+    app.get("/about", function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/views/about.html'));
+    });
+
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, '../pubic/views/contact.html'));
+    });
+
+
 };
+
